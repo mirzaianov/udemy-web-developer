b697e0f1ebc8b74bdcfa49f6b94610171a98add8
msejak <7o.mseJak@gmail.com>
2023-02-07 19:09:49 +0300

    lesson 70

--- gulpfile.js
+++ gulpfile.js
@@ -9,32 +9,32 @@ gulp.task('server', function () {
     browserSync({
         server: {
             // TODO: поменять путь!
-            baseDir: '69/pulse/src',
+            baseDir: '70/pulse/src',
         },
     });
 
     // TODO: поменять путь!
-    gulp.watch('69/pulse/src/*.html').on('change', browserSync.reload);
+    gulp.watch('70/pulse/src/*.html').on('change', browserSync.reload);
 });
 
 gulp.task('styles', function () {
     return (
         gulp
             // TODO: поменять путь!
-            .src('69/pulse/src/sass/**/*.+(scss|sass)')
+            .src('70/pulse/src/sass/**/*.+(scss|sass)')
             .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
             .pipe(rename({ suffix: '.min', prefix: '' }))
             .pipe(autoprefixer())
             .pipe(cleanCSS({ compatibility: 'ie8' }))
             // TODO: поменять путь!
-            .pipe(gulp.dest('69/pulse/src/css'))
+            .pipe(gulp.dest('70/pulse/src/css'))
             .pipe(browserSync.stream())
     );
 });
 
 gulp.task('watch', function () {
     // TODO: поменять путь!
-    gulp.watch('69/pulse/src/sass/**/*.+(scss|sass)', gulp.parallel('styles'));
+    gulp.watch('70/pulse/src/sass/**/*.+(scss|sass)', gulp.parallel('styles'));
 });
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
