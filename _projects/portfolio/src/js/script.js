2dad86fe56e6d83b14d3f0fb6b623073b81c4f0b
msejak <7o.mseJak@gmail.com>
2023-07-18 16:20:39 +0300

    lesson 86

--- _projects/portfolio/src/js/script.js
+++ _projects/portfolio/src/js/script.js
@@ -1,11 +1,18 @@
-const hamburger = document.querySelector(".hamburger"),
-  menu = document.querySelector(".menu"),
-  closeElem = document.querySelector(".menu__close");
+const hamburger = document.querySelector('.hamburger'),
+  menu = document.querySelector('.menu'),
+  closeElem = document.querySelector('.menu__close');
 
-hamburger.addEventListener("click", () => {
-  menu.classList.add("active");
+hamburger.addEventListener('click', () => {
+  menu.classList.add('active');
 });
 
-closeElem.addEventListener("click", () => {
-  menu.classList.remove("active");
+closeElem.addEventListener('click', () => {
+  menu.classList.remove('active');
+});
+
+const counters = document.querySelectorAll('.skills__percent'),
+  lines = document.querySelectorAll('.skills__indicator-full');
+
+counters.forEach((item, i) => {
+  lines[i].style.width = item.innerHTML;
 });
